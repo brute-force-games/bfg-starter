7dd8c8a73bca2838cb748c13f5faee0e1117d4d7
Albert Boehmler <ajboehmler@gmail.com>
2025-10-20 15:44:17 -0400

    initial bingo

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import { RouterProvider } from '@tanstack/react-router'
 import './index.css'
 import { router } from './router'
 import { initBasicGames } from '@bfg-gdp-basic-games'
+import { initPartyGames } from '@bfg-gdp-party-games'
 
 
 initBasicGames();
+initPartyGames();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import viteReact from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [
@@ -21,13 +16,6 @@ export default defineConfig({
   ],
   server: {
     port: 62776
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(dirname, './src'),
-      '@bfg-engine': path.resolve(dirname, './modules/bfg-engine/src'),
-      '@bfg-games': path.resolve(dirname, './modules/bfg-gdp-basic-games/src')
-    }
   },
   define: {
     BUILD_DETAILS: JSON.stringify({
